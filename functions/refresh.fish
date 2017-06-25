5ddcaa1494676e5d3aa07ef999cec6c81d7120db
shinriyo <shinriyo@shinriyos-MacBook.local>
2017-06-25 16:26:18 +0900

    fix command

--- functions/refresh.fish
+++ functions/refresh.fish
@@ -1,2 +1,4 @@
-networksetup -setairportnetwork en0 (airport -I | /usr/bin/grep -ie '^\s*ssid' | awk '{print $2}')
+function refresh
+    networksetup -setairportnetwork en0 (airport -I | /usr/bin/grep -ie '^\s*ssid' | awk '{print $2}')
+end
 
